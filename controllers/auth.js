fd6720e411264621c687db3eb2228a2d0008fdd0
Nawar Anzara <1830256@iub.edu.bd>
2021-09-08 03:54:56 +0600

    login done

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,6 +13,59 @@ const db = mysql.createConnection({
     database: process.env.DATABASE
 });
 
+exports.login = function (req, res) {
+    console.log(req.body);
+    const UserID = req.body.UserID;
+    module.exports.UserID = UserID;
+
+    const Password = req.body.Password;
+    
+    if (!UserID || !Password) {
+        return res.status(400).render('login', {
+            message: 'Please provide an ID or password'
+        })
+    }
+
+    db.query("SELECT UserID, Name, DescribeYourself, PictureUrl, AvatarName, sex, AgeGroup, Password from users WHERE UserID = ?", [UserID], async function (error, results) {
+        if (error) {
+            console.log(error);
+        }
+        console.log(results);
+        module.exports.UserID = results[0].UserID;
+        module.exports.Name = results[0].Name;
+        module.exports.DescribeYourself = results[0].DescribeYourself;
+        module.exports.PictureUrl = results[0].PictureUrl;
+        module.exports.AvatarName = results[0].AvatarName;
+        module.exports.sex = results[0].sex;
+        module.exports.Agegroup = results[0].AgeGroup;
+      
+         if (!(results) || !(await bcrypt.compare(Password, results[0].Password))) {
+            res.status(401).render('login', {
+                message: 'ID or Password is incorrect'
+            });
+        } else{
+             res.render("\index", {
+                 UserID: results[0].UserID,
+                 Name: results[0].Name,
+                 DescribeYourself: results[0].DescribeYourself,
+                 PictureUrl: results[0].PictureUrl,
+                 AvatarName: results[0].AvatarName,
+                 sex: results[0].sex,
+                 AgeGroup: results[0].AgeGroup
+
+             });
+        }
+
+    });
+
+        
+
+
+    
+
+
+}
+
 exports.register = function (req, res) {
     console.log(req.body);
     const { UserID, Name, DescribeYourself, PictureUrl, AvatarName, sex, AgeGroup, Password, confirm_password } = req.body;
